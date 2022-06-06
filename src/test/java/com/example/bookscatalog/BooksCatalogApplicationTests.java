b887569911c7696f4b1c6424cfe507af791b1ccd
patsayvlad <patsay2002@gmail.com>
2022-06-07 00:42:10 +0300

    Initial commit

---
+++ src/test/java/com/example/bookscatalog/BooksCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksCatalogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
